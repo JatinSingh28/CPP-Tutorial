b8efcc9748c81f62859846eff729ab7ddcfdc3e5
Jatin Singh Sagoi <sagoisinghjatin9951@gmail.com>
2021-10-02 17:22:11 +0530

    .

--- 13_str_unions_enums.cpp
+++ 13_str_unions_enums.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 typedef struct employee
 {
@@ -7,7 +7,8 @@ typedef struct employee
     float salary;
 } ep;
 
-union money{
+union money
+{
     int rice;
     char car;
     float pounds;
@@ -15,38 +16,40 @@ union money{
 
 //uniom provide better memory management than struct
 
-int main(){
+int main()
+{
     struct employee harry;
     ep shubham;
     shubham.ID = 99;
     harry.ID = 1;
     harry.favchar = 'c';
     harry.salary = 12000;
-    
-    cout<<harry.salary<<endl;
-    cout<<harry.ID<<endl;
-    cout<<harry.favchar<<endl;
-    cout<<shubham.ID<<endl;
+
+    cout << harry.salary << endl;
+    cout << harry.ID << endl;
+    cout << harry.favchar << endl;
+    cout << shubham.ID << endl;
 
     union money m1;
     m1.rice = 34;
     //m1.car = 'd';
-    cout<<m1.rice<<endl;
+    cout << m1.rice << endl;
     //cout<<m1.car;
     //use only one at a time this saves some memory
 
     //enum
 
-    enum meal{breakfast, lunch, dinner};
-    meal m2= lunch;
-    cout<<(m2 == 1
-)<<endl;
-    cout<<breakfast
-    
-    <<endl;
-    cout<<lunch<<endl;
-    cout<<dinner<<endl;
-
+    enum meal
+    {
+        breakfast,
+        lunch,
+        dinner
+    };
+    meal m2 = lunch;
+    cout << (m2 == 1) << endl;
+    cout << breakfast << endl;
+    cout << lunch << endl;
+    cout << dinner << endl;
 
     return 0;
 }
--- 15_call_by_value_and_reference.cpp
+++ 15_call_by_value_and_reference.cpp
@@ -1,22 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sum(int a, int b){
-    int c=a+b;
+int sum(int a, int b)
+{
+    int c = a + b;
     return c;
 }
 
 //void swap(int a, int b){ //temp  a  b
-    //int temp =a;        //4     4  5
-    //a=b;                //4     5  5
-    //b =temp;            //4     5  4
+//int temp =a;        //4     4  5
+//a=b;                //4     5  5
+//b =temp;            //4     5  4
 //}//this will not swap a and b
 
 //call by reference by pointers
-void swappointer(int* a, int* b){ //temp  a  b
-    int temp =*a;        //4     4  5
-    *a=*b;                //4     5  5
-    *b =temp;            //4     5  4
+void swappointer(int *a, int *b)
+{                  //temp  a  b
+    int temp = *a; //4     4  5
+    *a = *b;       //4     5  5
+    *b = temp;     //4     5  4
 }
 
 //call by reference by pointers
@@ -26,23 +28,25 @@ void swappointer(int* a, int* b){ //temp  a  b
     b =temp;            //4     5  4
     return a;
 }*/
-void swaprefvar(int &a, int &b){ //temp  a  b
-    int temp =a;        //4     4  5
-    a=b;                //4     5  5
-    b =temp;            //4     5  4
+void swaprefvar(int &a, int &b)
+{                 //temp  a  b
+    int temp = a; //4     4  5
+    a = b;        //4     5  5
+    b = temp;     //4     5  4
 }
 
-int main(){
-    int x=4, y=5;
+int main()
+{
+    int x = 4, y = 5;
     //cout<<"the sum of 4 and 5 is: "<<sum(x,y);
-    cout<<"the value of x is:"<<x<<endl;
-    cout<<"the value of y is:"<<y<<endl;
-    
+    cout << "the value of x is:" << x << endl;
+    cout << "the value of y is:" << y << endl;
+
     //swap(x,y);//this will not swap a and b
     //swappointer(&x,&y);//this will swap a and b using pointers reference
-    swaprefvar(x,y);//=766;//this will swap a and b using reference variable
-    
-    cout<<"the value of x is:"<<x<<endl;
-    cout<<"the value of y is:"<<y<<endl;
+    swaprefvar(x, y); //=766;//this will swap a and b using reference variable
+
+    cout << "the value of x is:" << x << endl;
+    cout << "the value of y is:" << y << endl;
     return 0;
 }
